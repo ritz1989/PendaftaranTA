277262ac50687afeec9d2e40bfe3c7142bece50c
ritz1989 <sayekti.harits@gmail.com>
2017-12-14 01:59:44 +0700

    aaa

--- PendaftaranTA/PendaftaranTA/views.py
+++ PendaftaranTA/PendaftaranTA/views.py
@@ -3,9 +3,10 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, escape, redirect
 from PendaftaranTA import app
 
+app.secret_key = 'gua kece karna gua kece'
 
 @app.route('/')
 @app.route('/home')
@@ -47,12 +48,82 @@ def loginForm():
         message='Your application description page.'
     )
 
-@app.route('/actionLogin', methods=['GET','POST'])
-def actionLogin():
-    data =  request.form['exampleInputEmail1']
+@app.route('/approve-dosen')
+def approve_dosen():
+    """Renders the about page."""
     return render_template(
-        'about.html',
-        title=data,
+        'approve_dosen.html',
+        title='About',
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/biodata-dosen')
+def biodata_dosen():
+    """Renders the about page."""
+    return render_template(
+        'biodata_dosen.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/biodata-mhs')
+def biodata_mhs():
+    """Renders the about page."""
+    return render_template(
+        'biodata_mhs.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/input-ta')
+def input_ta():
+    """Renders the about page."""
+    return render_template(
+        'input_ta.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/lab')
+def lab():
+    """Renders the about page."""
+    return render_template(
+        'lab.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/ta')
+def ta():
+    """Renders the about page."""
+    return render_template(
+        'ta.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/actionLogin', methods=['POST'])
+def actionLogin():
+    if request.method=='POST':
+        username =  request.form['username']
+        password = request.form['password']
+        loginAs = request.form['asRadio']
+        session['username'] = username
+        session['login'] = loginAs
+        return redirect(url_for('home'))
+    return render_template(
+        'about.html',
+        title='fail',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+def clearSession():
+    session.pop('username', None)
+    session.pop('login', None)
